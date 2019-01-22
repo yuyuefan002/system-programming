4f338db8c26005d941468133766d5913114ffdb1
yuyuefan002 <yy225@vcm-5475.vm.duke.edu>
2019-01-21 21:22:20 -0500

    add some test case

--- my_malloc/general_tests/mymalloc_test.c
+++ my_malloc/general_tests/mymalloc_test.c
@@ -1,19 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "my_malloc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifdef FF
 #define MALLOC(sz) ff_malloc(sz)
-#define FREE(p)    ff_free(p)
+#define FREE(p) ff_free(p)
 #endif
 #ifdef BF
 #define MALLOC(sz) bf_malloc(sz)
-#define FREE(p)    bf_free(p)
+#define FREE(p) bf_free(p)
 #endif
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   const unsigned NUM_ITEMS = 10;
   int i;
   int size;
@@ -24,65 +22,127 @@ int main(int argc, char *argv[])
   size = 4;
   expected_sum += size * size;
   array[0] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[0][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[0][i];
-  } //for i
+  } // for i
 
   size = 16;
   expected_sum += size * size;
   array[1] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[1][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[1][i];
-  } //for i
+  } // for i
 
   size = 8;
   expected_sum += size * size;
   array[2] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[2][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[2][i];
-  } //for i
+  } // for i
 
   size = 32;
   expected_sum += size * size;
   array[3] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[3][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[3][i];
-  } //for i
+  } // for i
 
   FREE(array[0]);
   FREE(array[2]);
 
+  size = 4;
+  expected_sum += size * size;
+  array[0] = (int *)MALLOC(size * sizeof(int));
+  for (i = 0; i < size; i++) {
+    array[0][i] = size;
+  } // for i
+  for (i = 0; i < size; i++) {
+    sum += array[0][i];
+  } // for i
+  FREE(array[0]);
+  printf("bigger\n");
+  size = 5;
+  expected_sum += size * size;
+  array[0] = (int *)MALLOC(size * sizeof(int));
+  for (i = 0; i < size; i++) {
+    array[0][i] = size;
+  } // for i
+  for (i = 0; i < size; i++) {
+    sum += array[0][i];
+  } // for i
+  FREE(array[0]);
+  printf("smaller\n");
+  size = 3;
+  expected_sum += size * size;
+  array[0] = (int *)MALLOC(size * sizeof(int));
+  for (i = 0; i < size; i++) {
+    array[0][i] = size;
+  } // for i
+  for (i = 0; i < size; i++) {
+    sum += array[0][i];
+  } // for i
+  FREE(array[0]);
+  size = 8;
+  expected_sum += size * size;
+  array[2] = (int *)MALLOC(size * sizeof(int));
+  for (i = 0; i < size; i++) {
+    array[2][i] = size;
+  } // for i
+  for (i = 0; i < size; i++) {
+    sum += array[2][i];
+  } // for i
+  FREE(array[2]);
+  size = 8;
+  expected_sum += size * size;
+  array[2] = (int *)MALLOC(size * sizeof(int));
+  for (i = 0; i < size; i++) {
+    array[2][i] = size;
+  } // for i
+  for (i = 0; i < size; i++) {
+    sum += array[2][i];
+  } // for i
+  FREE(array[2]);
+  size = 8;
+  expected_sum += size * size;
+  array[2] = (int *)MALLOC(size * sizeof(int));
+  for (i = 0; i < size; i++) {
+    array[2][i] = size;
+  } // for i
+  for (i = 0; i < size; i++) {
+    sum += array[2][i];
+  } // for i
+  FREE(array[2]);
   size = 7;
   expected_sum += size * size;
   array[4] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[4][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[4][i];
-  } //for i
+  } // for i
 
   size = 256;
   expected_sum += size * size;
   array[5] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[5][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[5][i];
-  } //for i
+  } // for i
 
   FREE(array[5]);
   FREE(array[1]);
@@ -91,44 +151,44 @@ int main(int argc, char *argv[])
   size = 23;
   expected_sum += size * size;
   array[6] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[6][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[6][i];
-  } //for i
+  } // for i
 
   size = 4;
   expected_sum += size * size;
   array[7] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[7][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[7][i];
-  } //for i
+  } // for i
 
   FREE(array[4]);
 
   size = 10;
   expected_sum += size * size;
   array[8] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[8][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[8][i];
-  } //for i
+  } // for i
 
   size = 32;
   expected_sum += size * size;
   array[9] = (int *)MALLOC(size * sizeof(int));
-  for (i=0; i < size; i++) {
+  for (i = 0; i < size; i++) {
     array[9][i] = size;
-  } //for i
-  for (i=0; i < size; i++) {
+  } // for i
+  for (i = 0; i < size; i++) {
     sum += array[9][i];
-  } //for i
+  } // for i
 
   FREE(array[6]);
   FREE(array[7]);
@@ -141,7 +201,8 @@ int main(int argc, char *argv[])
   } else {
     printf("Expected sum=%d but calculated %d\n", expected_sum, sum);
     printf("Test failed\n");
-  } //else
-
+  } // else
+  printf("total block size: %ld\n", get_data_segment_size());
+  printf("total free block size: %ld\n", get_data_segment_free_space_size());
   return 0;
 }
